ff511eeae40405598cbd2b02f092f660ed293081
Mohammed Ali <mohaali482@gmail.com>
2023-07-12 18:20:31 +0300

    Add solution for running sum

---
+++ leetcode/runningSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        prefix = [nums[0]]
+        for i in nums[1:]:
+            prefix.append(prefix[-1] + i)
+
+        return prefix
+
+
+# 1min
