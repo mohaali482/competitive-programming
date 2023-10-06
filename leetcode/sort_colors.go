38c8191fbc382170b0db4a2355f21b65c90eea02
Mohammed Ali <mohaali482@gmail.com>
2023-10-06 17:14:57 +0300

    Add solution for sort colors in go

---
+++ leetcode/sort_colors.go
@@ -0,0 +1,14 @@
+func sortColors(nums []int) {
+	var key, j int
+
+	for i := 1; i < len(nums); i++ {
+		key = nums[i]
+		j = i - 1
+		for j >= 0 && key < nums[j] {
+			nums[j+1] = nums[j]
+			j -= 1
+		}
+		nums[j+1] = key
+	}
+
+}
