c3abf88b261cff9f4746936ccc96fca7a6b4bbc9
Mohammed Ali <mohaali482@gmail.com>
2023-11-19 10:24:31 +0300

    Add solution for Merging arrays

---
+++ codeforces/A_Merging_Arrays.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+i = 0
+j = 0
+ans = []
+while i < n and j < m:
+    if a[i] < b[j]:
+        ans.append(a[i])
+        i += 1
+    else:
+        ans.append(b[j])
+        j += 1
+
+ans.extend(a[i:])
+ans.extend(b[j:])
+
+print(*ans)
