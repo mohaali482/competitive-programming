5900137a0a361d835379aef293223710d06e7f2f
Mohammed Ali <mohaali482@gmail.com>
2023-12-02 18:28:39 +0300

    Add solution for cube conundrum

--- advent-of-code-2023/main.go
+++ advent-of-code-2023/main.go
@@ -6,14 +6,16 @@ import (
 	"os"
 	"strconv"
 
+	cubeconundrum "github.com/mohaali482/competitive-programming/advent_of_code_2023/cube_conundrum"
 	"github.com/mohaali482/competitive-programming/advent_of_code_2023/trebuchet"
 )
 
 func main() {
-	trebuchet_solution()
+	// trebuchet_solution()
+	cube_conundrum()
 }
 
-func trebuchet_solution() {
+func _trebuchet_solution() {
 	file, err := os.Open("trebuchet/input.txt")
 	if err != nil {
 		panic(err)
@@ -24,6 +26,23 @@ func trebuchet_solution() {
 	var num int
 	for scanner.Scan() {
 		num, _ = strconv.Atoi(trebuchet.Solution2(scanner.Text()))
+		ans += num
+	}
+
+	fmt.Println(ans)
+}
+
+func cube_conundrum() {
+	file, err := os.Open("cube_conundrum/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	ans := 0
+	var num int
+	for scanner.Scan() {
+		num = cubeconundrum.Solution(scanner.Text())
 		ans += num
 	}
 
