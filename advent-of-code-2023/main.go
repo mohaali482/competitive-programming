8e88619c4f50b2ba5ba20db8241e925465ae0dc0
Mohammed Ali <mohaali482@gmail.com>
2023-12-02 18:32:54 +0300

    Comment out unused code

--- advent-of-code-2023/main.go
+++ advent-of-code-2023/main.go
@@ -4,10 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 
 	cubeconundrum "github.com/mohaali482/competitive-programming/advent_of_code_2023/cube_conundrum"
-	"github.com/mohaali482/competitive-programming/advent_of_code_2023/trebuchet"
 )
 
 func main() {
@@ -16,39 +14,39 @@ func main() {
 	cube_conundrum2()
 }
 
-func _trebuchet_solution() {
-	file, err := os.Open("trebuchet/input.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	ans := 0
-	var num int
-	for scanner.Scan() {
-		num, _ = strconv.Atoi(trebuchet.Solution2(scanner.Text()))
-		ans += num
-	}
+// func trebuchet_solution() {
+// 	file, err := os.Open("trebuchet/input.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer file.Close()
+// 	scanner := bufio.NewScanner(file)
+// 	ans := 0
+// 	var num int
+// 	for scanner.Scan() {
+// 		num, _ = strconv.Atoi(trebuchet.Solution2(scanner.Text()))
+// 		ans += num
+// 	}
 
-	fmt.Println(ans)
-}
+// 	fmt.Println(ans)
+// }
 
-func cube_conundrum() {
-	file, err := os.Open("cube_conundrum/input.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	ans := 0
-	var num int
-	for scanner.Scan() {
-		num = cubeconundrum.Solution(scanner.Text())
-		ans += num
-	}
+// func cube_conundrum() {
+// 	file, err := os.Open("cube_conundrum/input.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer file.Close()
+// 	scanner := bufio.NewScanner(file)
+// 	ans := 0
+// 	var num int
+// 	for scanner.Scan() {
+// 		num = cubeconundrum.Solution(scanner.Text())
+// 		ans += num
+// 	}
 
-	fmt.Println(ans)
-}
+// 	fmt.Println(ans)
+// }
 
 func cube_conundrum2() {
 	file, err := os.Open("cube_conundrum/input.txt")
