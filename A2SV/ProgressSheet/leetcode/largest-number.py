25ab9780ce23945b66fb308cc364f2be405533c1
Mohammed Ali <68723236+mohaali482@users.noreply.github.com>
2023-12-29 19:36:55 +0300

    Added solution for Largest Number

---
+++ A2SV/ProgressSheet/leetcode/largest-number.py
@@ -0,0 +1,17 @@
+from functools import cmp_to_key as k
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def compare(x, y):
+            if int(x+y)>int(y+x):
+                return -1
+            elif int(x+y) < int(y+x):
+                return 1
+            return 0
+        a = []
+        for i in nums:
+            a.append(str(i))
+        a.sort(key=k(compare))
+        return str(int(''.join(a))) 
+
+
+        
