db0b1739aac42e9c5d4e759b83cf3372cd67a8fb
Mohammed Ali <68723236+mohaali482@users.noreply.github.com>
2024-03-01 11:07:06 +0300

    Add solution for Subsets

--- A2SV/ProgressSheet/leetcode/leetcode/subsets.py
+++ A2SV/ProgressSheet/leetcode/leetcode/subsets.py
@@ -1,11 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
+        ans = []
         current = []
-        def generate(i):
-            for i in range(i, len(nums)):
+        def generate(idx):
+            ans.append(current[:])
+            for i in range(idx, len(nums)):
                 current.append(nums[i])
-                ans.append(current.copy())
                 generate(i+1)
                 current.pop()
         generate(0)
